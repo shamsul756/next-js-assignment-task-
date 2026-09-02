88bb3cdcffe0b44d9c1eea295c79f04f1a4f1fdc
shamsul765 <webdevlopershamsul@gmail.com>
2026-09-02 21:39:34 +0600

    solved problem 3 using if , else if

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function descriveValue(value){
-    let type = typeof value;
-    let typeCheck = value ? "truthy" : "falsy";
- 
-    return type + " | " + typeCheck;
+function validUserName(userName){
+    if(userName.length < 3){
+        return "too short"
+    }
+  else if(userName.includes(" ")){
+    return "space are not allowed"
+  }
+
+  else if(userName.toLowerCase().includes("admin")){
+    return "reserved keyword"
+  }
+  return "available"
 }
-let checkValue = descriveValue(3);
+let checkValue = validUserName("adin");
 console.log(checkValue);
