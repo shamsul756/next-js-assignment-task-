fc0c5888d30fe7b33a56bc6262cf7ecf7923461c
shamsul765 <webdevlopershamsul@gmail.com>
2026-09-02 23:10:57 +0600

    problem 2 are solved

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-const getChaseVerdict = (target, scored, ballsLeft) => {
-    const runsNeeded = target - scored;
+const getDayType = (day) => {
+    day = day.toLowerCase();
 
-    if (runsNeeded <= 0) {
-        return "Won";
+    switch (day) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
     }
-
-    if (ballsLeft <= 0) {
-        return "Lost";
-    }
-
-    const requiredRate = (runsNeeded / ballsLeft) * 6;
-
-    let verdict;
-
-    if (requiredRate <= 6) {
-        verdict = "Comfortable";
-    } else if (requiredRate <= 12) {
-        verdict = "Tough";
-    } else {
-        verdict = "Almost Impossible";
-    }
-
-    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
 };
-console.log(getChaseVerdict(100, 90, 12));
-// Need 10 runs in 12 balls | Comfortable
-
-console.log(getChaseVerdict(100, 80, 12));
-// Need 20 runs in 12 balls | Tough
-
-console.log(getChaseVerdict(100, 70, 12));
-// Need 30 runs in 12 balls | Almost Impossible
